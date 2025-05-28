56aa250fa9fc47c0c0b2ea1095c87852bbdd7842
Martinez <edwin.martinez@softwareone.com>
2025-05-28 11:26:47 -0500

    second commit 28-05-2025 creacion de clase orden de ejecucion

---
+++ src/com/empresa/OrdenDeEjecucion.java
@@ -0,0 +1,18 @@
+package com.empresa;
+
+public class OrdenDeEjecucion {
+
+	public static void main(String[] args) {
+		String var1 = "Hola";
+		String var2 = "Equipo";
+		
+		int a = 200;
+		int b = 100;
+		System.out.println(var1+" "+var2);
+		System.out.println(a+b);
+		
+		System.out.println(a + b + var1 + var2);
+		System.out.println(var1 + var2 + a + b);
+	}
+
+}
