8809dcb4051c5c892d1cd79bffd29674bf78434a
Martinez <edwin.martinez@softwareone.com>
2025-06-03 09:09:05 -0500

    Commit 4 03-06-25 se creo la clase switch

---
+++ src/com/empresa/SwitchCase.java
@@ -0,0 +1,35 @@
+package com.empresa;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+			String modulo  = "Contabilidad";
+			switch(modulo) {
+			case "Contabilidad": 	
+				System.out.println("Ingreso al modulo de contabilidad");
+				break;
+			case "Tesoreria":
+				System.out.println("Ingreso al modulo de tesoreria");	
+				break;
+			case "Nomina": 
+				System.out.println("Ingreso al modulo de nomina");
+				break;
+			case "Inventarios":
+				System.out.println("Ingreso al modulo de Inventarios");
+				break;
+			case "Tndycon":
+				System.out.println("Ingreso al modulo de Indicom");
+				break;
+			case "Predial":
+				System.out.println("Ingreso al modulo de Predial	");
+				break;
+			case "Ejemplo":
+				System.out.println("Ingreso al modulo de Ejemplo");
+				break;
+			default:
+				System.out.println("Ni monda");
+				
+			}	
+	}
+
+}
